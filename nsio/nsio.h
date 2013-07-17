11c6ea24396134221cf0936d338b52fd2e30ebf0
TwistedUmbrella <TwistedUmbrella@gmail.com>
2013-07-17 15:33:59 -0400

    [nsio] Deprecated definitions (linux/kernel.h), Update ipv4

--- nsio/nsio.h
+++ nsio/nsio.h
@@ -8,6 +8,21 @@
 
 #define MAX_DATA_LEN          	1024
 
+#define NIPQUAD(addr) \
+((unsigned char *)&addr)[0], \
+((unsigned char *)&addr)[1], \
+((unsigned char *)&addr)[2], \
+((unsigned char *)&addr)[3]
+
+#define NIP6(addr) \
+ntohs((addr).s6_addr16[0]), \
+ntohs((addr).s6_addr16[1]), \
+ntohs((addr).s6_addr16[2]), \
+ntohs((addr).s6_addr16[3]), \
+ntohs((addr).s6_addr16[4]), \
+ntohs((addr).s6_addr16[5]), \
+ntohs((addr).s6_addr16[6]), \
+ntohs((addr).s6_addr16[7])
 
 typedef struct {
     struct timespec ctime;
@@ -57,5 +72,5 @@ enum {
 #define ACT_LOG     0x100
 #define ACT_IGNORE  0x00
 #define ACT_DROP    0x01
-#define ACT_ALTER   0x02 
+#define ACT_ALTER   0x02
 
